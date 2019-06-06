378166a82dd62a2bd8ee9c89523ff2cf1478d225
Raí Siqueira <rai93siqueira@gmail.com>
2019-06-05 22:44:12 -0300

    Example for version 2 for docker stack
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
-			"version": "1.0.0",
+			"message": "pong on version 2",
+			"version": "2.0.0",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
